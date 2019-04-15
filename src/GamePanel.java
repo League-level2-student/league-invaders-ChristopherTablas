03c53cb1dfcf8ed0bcefae935cfa65263554cba0
ChristopherTablas <christopher.tablasdelacruz@gompersstudent.org>
2019-04-15 14:26:38 -0700

    41519
    leaguinvaders part 1-4

---
+++ src/GamePanel.java
@@ -0,0 +1,50 @@
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class GamePanel extends JPanel implements ActionListener, KeyListener{
+		Timer time;
+		GamePanel(){
+			time = new Timer(1000/60, this);
+		}
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			repaint();
+			// TODO Auto-generated method stub
+			
+		}
+		 @Override
+
+		 public void paintComponent(Graphics g){
+			 g.fillRect(10, 10, 100, 100);
+		                 
+
+		         }
+	void startGame(){
+		time.start();
+		
+		}
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		System.out.println("test 3");
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		System.out.println("test 2");
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		System.out.println("test 1");
+		// TODO Auto-generated method stub
+		
+	}
+	}
--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,43 @@
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class LeagueInvaders {
-
+	private static final Component KeyListener = null;
+	JFrame frame;
+	final int wi = 500;
+	final int he = 800;
+	Timer time;
+	GamePanel gpan;
+	public static void main(String[] args) {
+		LeagueInvaders league = new LeagueInvaders();
+		league.setup();
+	}
+	LeagueInvaders(){
+		frame = new JFrame();
+		frame.setSize(wi, he);
+		gpan = new GamePanel();
+	}
+	void setup(){
+	
+	frame.add(gpan);
+	frame.getContentPane().setPreferredSize(new Dimension(wi, he));
+	
+    frame.pack();
+	frame.setVisible(true);
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	gpan.startGame();
+	}
+	
+	private void setDefaultCloseOperation(int exitOnClose) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
