1ad0bcb57c9630e58da66cdfb842f95280e937f0
ChristopherTablas <christopher.tablasdelacruz@gompersstudent.org>
2019-04-18 14:30:31 -0700

    Leagueinvadersday3

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,23 +11,41 @@ import javax.swing.Timer;
 public class GamePanel extends JPanel implements ActionListener, KeyListener{
 		Timer time;
 		GameObject gameobj;
+		final int MENU_STATE = 0;
+		final int GAME_STATE = 1;
+		final int END_STATE = 2;
+		int currentState = MENU_STATE;
 		GamePanel(){
 			time = new Timer(1000/60, this);
 			gameobj = new GameObject(1,1,1,1);
 		}
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
-			
 			gameobj.update();
 			repaint();
+			if(currentState == MENU_STATE){
+                updateMenuState();
+        }else if(currentState == GAME_STATE){
+                updateGameState();
+        }else if(currentState == END_STATE){
+                updateEndState();
+        }
+
+
 			// TODO Auto-generated method stub
 			
 		}
 		 @Override
 
 		 public void paintComponent(Graphics g){
-			 
 		         gameobj.draw(g);        
+		         if(currentState == MENU_STATE){
+		                drawMenuState(g);
+		        }else if(currentState == GAME_STATE){
+		                drawGameState(g);
+		        }else if(currentState == END_STATE){
+		                drawEndState(g);
+		        }
 
 		         }
 	void startGame(){
@@ -51,4 +70,27 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener{
 		// TODO Auto-generated method stub
 		
 	}
+	void updateMenuState(){
+		
+		
+	}
+	void updateGameState(){
+		
+		
+	}
+	void updateEndState(){
+		
+	}
+	void drawMenuState(Graphics g){
+		g.setColor(Color.BLUE);
+
+		g.fillRect(0, 0, 500, 800);    
+	}
+	void drawGameState(Graphics g){
+		g.setColor(Color.BLACK);
+	}
+	void drawEndState(Graphics g){
+		g.setColor(Color.RED);
+	}
+	
 	}
